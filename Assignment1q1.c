dc84adcea1b9126fc0fcfad7ffb9730e91df1251
Zain-Fatani <144994692+Zain-Fatani@users.noreply.github.com>
2023-12-06 01:51:59 +0300

    Update Assignment1q1.c

--- Assignment1q1.c
+++ Assignment1q1.c
@@ -10,14 +10,14 @@ int main(int argc, char *argv[]){
     int l = 0;
    
     for ( i = 0; i < 26; i++) {
-        pw[0] = letters[i];
         for ( j = 0; j < 26; j++) {
-            pw[1] = letters[j];
-            for ( k = 0; k < 26; k++) {
-                pw[2] = letters[k];
-                for ( l = 0; l < 26; l++) {
-                    pw[3] = letters[l];
+            for ( k = 0; k < 26; k++) {                
+                for ( l = 0; l < 26; l++) {                    
                     if (i != j && i != k && i != l && j != k && j != l && k != l) {
+                        pw[0] = letters[i];
+                        pw[1] = letters[j];
+                        pw[2] = letters[k];
+                        pw[3] = letters[l];
                         printf("%c%c%c%c\n", pw[0],pw[1],pw[2],pw[3]);
                     }
                 }
